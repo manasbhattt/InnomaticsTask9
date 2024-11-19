e68faecf8b0eb4addc2e0f5f66cd6b7083becf83
Manas Mrinal Bhatt <139228474+manasbhattt@users.noreply.github.com>
2024-11-20 01:20:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000; 
 
 app.use(express.json());
 
-
-mongoose.connect('mongodb+srv://mongodb:manasdb@cluster0.a6t31xg.mongodb.net/CRUD-api', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Failed to connect to MongoDB', err);
-});
+mongoose
+  .connect('mongodb+srv://mongodb:manasdb@cluster0.a6t31xg.mongodb.net/CRUD-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
 
 const todoRoutes = require('./routes/todos');
 app.use('/todos', todoRoutes);
